bbd3b4d5bdcb16acb6565ef96c9880e872d7feac
WANG QIANG <rovingbreeze@gmail.com>
2016-04-10 16:36:57 +0800

    Proper handle code blocks

--- coursemology2/db/transforms/lib/content_parser.rb
+++ coursemology2/db/transforms/lib/content_parser.rb
@@ -2,10 +2,10 @@ class ContentParser
   def self.parse_mc_tags(content)
     return content unless content.present?
 
-    content.gsub!('[mc]', '<pre class="codehilite codehilite-python"><code>')
+    content.gsub!('[mc]', '<pre lang="python"><code>')
     content.gsub!('[/mc]', '</code></pre>')
 
-    content.gsub!('[c]', '<pre class="codehilite codehilite-python"><code>')
+    content.gsub!('[c]', '<pre lang="python"><code>')
     content.gsub!('[/c]', '</code></pre>')
     content
   end
