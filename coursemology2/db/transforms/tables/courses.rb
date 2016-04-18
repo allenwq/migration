54a8025b05a2e1e76de7e7f62321234f0dd8a673
WANG QIANG <rovingbreeze@gmail.com>
2016-04-18 17:17:26 +0800

    Handle course settings, Fix #4

--- coursemology2/db/transforms/tables/courses.rb
+++ coursemology2/db/transforms/tables/courses.rb
@@ -1,6 +1,4 @@
 def transform_courses(ids = [])
-  ids = Array(ids)
-
   transform_table :courses, to: ::Course, default_scope: proc { where(id: ids) } do
     primary_key :id
     column to: :creator_id do
@@ -27,6 +25,7 @@ def transform_courses(ids = [])
     end
 
     column to: :assessment_categories do
+      migrate_settings(source_record, self)
       build_assessment_categories(source_record, self)
     end
 
@@ -72,6 +71,58 @@ def build_assessment_categories(source, destination)
     updated_at: mission_pref.updated_at
   )
   [default_category, mission_category]
+end
+
+def migrate_settings(source, destination)
+  mapping = {
+    'announcements' => { sidebar_key: :announcements, # Key for sidebar settings
+                         component_settings_key: :announcement, # Key for concrete component settings
+                         component_key: :course_announcements_component, # Component index settings
+                         default_name: 'Announcements'},
+    'achievements' => { sidebar_key: :achievements,
+                        component_settings_key: nil,
+                        component_key: :course_achievements_component,
+                        default_name: 'Achievements'},
+    'leaderboard' => { sidebar_key: :leaderboard,
+                       component_settings_key: :leaderboard,
+                       component_key: :course_leaderboard_component,
+                       default_name: 'Leaderboard'},
+    'students' => { sidebar_key: :users,
+                    component_settings_key: nil,
+                    component_key: nil,
+                    default_name: 'Students'},
+    'lesson_plan' => { sidebar_key: :lesson_plan,
+                       component_settings_key: nil,
+                       component_key: :course_lesson_plan_component,
+                       default_name: 'Lesson Plan'},
+    'materials' => { sidebar_key: :materials,
+                     component_settings_key: :material,
+                     component_key: :course_materials_component,
+                     default_name: 'Materials'},
+    'forums' => { sidebar_key: :forums,
+                  component_settings_key: :forum,
+                  component_key: :course_forums_component,
+                  default_name: 'Forums'}
+  }
+  source.course_navbar_preferences.each do |nav_pref|
+    key = nav_pref.item
+    next unless item_hash = mapping[key]
+
+    # Sidebar order
+    destination.settings(:sidebar, item_hash[:sidebar_key]).weight = nav_pref.pos
+
+    if component_settings_key = item_hash[:component_settings_key]
+      # Handle individual component settings here
+      if nav_pref.name != item_hash[:default_name]
+        destination.settings(component_settings_key).title = nav_pref.name
+      end
+    end
+
+    if component_key = item_hash[:component_key]
+      # Component enable/disable settings
+      destination.settings(:components, component_key).enabled = nav_pref.is_enabled
+    end
+  end
 end
 
 # V1:
