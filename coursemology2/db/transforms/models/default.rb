3d5c80b977e2806e91df97c469c91fd70e80d386
WANG QIANG <rovingbreeze@gmail.com>
2016-06-20 23:43:14 +0800

    Ignore attachment callbacks

--- coursemology2/db/transforms/models/default.rb
+++ coursemology2/db/transforms/models/default.rb
@@ -1,3 +1,9 @@
 module CoursemologyV1::Source
   def_model 'user_courses', 'announcements', 'levels'
+
+  ::Course::Announcement.class_eval do
+    # Ignore attachment callbacks.
+    def update_attachment_references
+    end
+  end
 end
