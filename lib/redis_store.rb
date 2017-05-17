b379158d2b5feae40af506302828c3d5f2e35a43
WANG QIANG <rovingbreeze@gmail.com>
2017-05-17 12:29:55 +0800

    Generalise redis store

--- lib/redis_store.rb
+++ lib/redis_store.rb
@@ -1,34 +1,38 @@
+# This is a wrapper of Redis that provides a namespace
 class RedisStore
+  def self.instance
+    @instance ||= self.new
+  end
+
   def initialize
     @store = Redis.new
   end
 
-  def get(table_name, key)
-    key = combined_key(table_name, key)
+  def get(namespace, key)
+    key = combined_key(namespace, key)
     @store.get(key)
   end
 
-  def set(table_name, key, value)
-    key = combined_key(table_name, key)
+  def set(namespace, key, value)
+    key = combined_key(namespace, key)
     @store.set(key, value)
   end
 
-  def has_key?(table_name, key)
-    key = combined_key(table_name, key)
+  def has_key?(namespace, key)
+    key = combined_key(namespace, key)
     @store.exists(key)
   end
 
-  # Clear all the data of specific table from Redis.
-  # This should be called before migrating the table.
-  def reset_table(table_name)
-    @store.keys([table_name].to_yaml + '*').each do |key|
+  # Clear all the data of specific namespace from Redis.
+  def reset(namespace)
+    @store.keys([namespace].to_yaml + '*').each do |key|
       @store.del(key)
     end
   end
 
   private
 
-  def combined_key(table_name, key)
-    [table_name, key].to_yaml
+  def combined_key(namespace, key)
+    [namespace, key].to_yaml
   end
 end
