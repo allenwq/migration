67f8ffac448237d05c833c85040b4613af7bb153
WANG QIANG <rovingbreeze@gmail.com>
2017-05-30 16:33:34 +0800

    Migrate activities

---
+++ models/activities.rb
@@ -0,0 +1,70 @@
+module V1
+  def_model 'activities' do
+    def target_actor_id(store)
+      target_course_user = store.get(UserCourse.table_name, actor_course_id)
+      CourseUser.find(target_course_user).user_id
+    end
+
+    def target_object(store)
+      # Possible source object types: ["Achievement", "Assessment", "Level", "ForumPost", "ForumTopic"]
+      # Possible target types: ["Course::Assessment", "Course::Forum::Topic", "Course::Discussion::Post",
+      # "Course::Level", "Course::Achievement", "Course::Announcement", "Course::Assessment::Submission", "Course::Video"]
+      target_type = case obj_type
+                    when 'Achievement'
+                      ::Course::Achievement.name
+                    when 'Assessment'
+                      ::Course::Assessment.name
+                    when 'Level'
+                      ::Course::Level.name
+                    when 'ForumPost'
+                      ::Course::Discussion::Post.name
+                    when 'ForumTopic'
+                      ::Course::Forum::Topic.name
+                    end
+
+      target_id = store.get("V1::#{obj_type}".constantize.table_name, obj_id)
+
+      [target_type, target_id]
+    end
+
+    def target_notifier_and_event
+      # Possible source combinations:
+      # {"Achievement"=>[[4, "earned"]]},
+      # {"Assessment"=>[[1, "attempted"], [7, "started"]]},
+      # {"Level"=>[[4, "earned"], [8, "reached"]]},
+      # {"ForumPost"=>[[6, "replied to"], [11, "voted on"]]},
+      # {"ForumTopic"=>[[9, "created Forum topic"], [10, "asked"]]}
+
+      # Possible target combinations
+      # ["Course::AssessmentNotifier", "attempted"],
+      # ["Course::Forum::TopicNotifier", "created"],
+      # ["Course::Forum::PostNotifier", "replied"],
+      # ["Course::LevelNotifier", "reached"],
+      # ["Course::AchievementNotifier", "gained"],
+      # ["Course::AnnouncementNotifier", "new"],
+      # ["Course::AssessmentNotifier", "submitted"],
+      # ["Course::Assessment::Answer::CommentNotifier", "annotated"],
+      # ["Course::Assessment::Answer::CommentNotifier", "replied"],
+      # ["Course::VideoNotifier", "attempted"],
+      # ["Course::VideoNotifier", "opening"],
+      # ["Course::Assessment::SubmissionQuestion::CommentNotifier", "replied"]
+      case [obj_type, action_id]
+      when ['Achievement', 4]
+        ['Course::AchievementNotifier', 'gained']
+      when ['Assessment', 1], ['Assessment', 7]
+        ['Course::AssessmentNotifier', 'attempted']
+      when ['Level', 4], ['Level', 8]
+        ['Course::LevelNotifier', 'reached']
+      when ['ForumPost', 6]
+        ['Course::Forum::PostNotifier', 'replied']
+      when ['ForumTopic', 9], ['ForumTopic', 10]
+        ['Course::Forum::TopicNotifier', 'created']
+      when ['ForumPost', 11]
+        ['Course::Forum::PostNotifier', 'voted']
+      else
+        raise "Unexpected type: #{obj_type} #{action_id}"
+      end
+    end
+  end
+end
+
---
+++ tables/activities.rb
@@ -0,0 +1,73 @@
+class ActivityTable < BaseTable
+  table_name 'activities'
+  scope { |ids| within_courses(ids) }
+
+  def migrate_batch(batch)
+    batch.each do |old|
+      new = ::Course::Notification.new
+      new.notification_type = :feed
+      activity = ::Activity.new
+      activity.actor_id = old.target_actor_id(store)
+      activity.object_type, activity.object_id = old.target_object(store)
+      activity.notifier_type, activity.event = old.target_notifier_and_event
+      activity.created_at = old.created_at
+      activity.updated_at = old.updated_at
+
+      migrate(old, new) do
+        column :activity do
+          activity
+        end
+        column :course_id do
+          store.get(V1::Course.table_name, old.course_id)
+        end
+        column :created_at
+        column :updated_at
+
+        skip_saving_unless_valid
+        store.set(model.table_name, old.id, new.id)
+      end
+    end
+  end
+end
+
+# Schema
+#
+# V2:
+# create_table "course_notifications", force: :cascade do |t|
+#   t.integer  "activity_id",       :null=>false, :index=>{:name=>"index_course_notifications_on_activity_id"}, :foreign_key=>{:references=>"activities", :name=>"fk_course_notifications_activity_id", :on_update=>:no_action, :on_delete=>:no_action}
+#   t.integer  "course_id",         :null=>false, :index=>{:name=>"index_course_notifications_on_course_id"}, :foreign_key=>{:references=>"courses", :name=>"fk_course_notifications_course_id", :on_update=>:no_action, :on_delete=>:no_action}
+#   t.integer  "notification_type", :default=>0, :null=>false
+#   t.datetime "created_at",        :null=>false
+#   t.datetime "updated_at",        :null=>false
+# end
+#
+# create_table "activities", force: :cascade do |t|
+#   t.integer  "actor_id",      :null=>false, :index=>{:name=>"fk__activities_actor_id"}, :foreign_key=>{:references=>"users", :name=>"fk_activities_actor_id", :on_update=>:no_action, :on_delete=>:no_action}
+#   t.integer  "object_id",     :null=>false
+#   t.string   "object_type",   :limit=>255, :null=>false
+#   t.string   "event",         :limit=>255, :null=>false
+#   t.string   "notifier_type", :limit=>255, :null=>false
+#   t.datetime "created_at",    :null=>false
+#   t.datetime "updated_at",    :null=>false
+# end
+
+# V1
+# create_table "activities", :force => true do |t|
+#   t.integer  "course_id"
+#   t.integer  "actor_course_id"
+#   t.integer  "target_course_id" comment: not used
+#   t.integer  "action_id"
+#   t.integer  "obj_id"
+#   t.string   "obj_type"
+#   t.string   "extra"  comment: not used
+#   t.datetime "created_at",       :null => false
+#   t.datetime "updated_at",       :null => false
+#   t.string   "obj_url"
+# end
+#
+# create_table "actions", :force => true do |t|
+#   t.string   "text"
+#   t.text     "description"
+#   t.datetime "created_at",  :null => false
+#   t.datetime "updated_at",  :null => false
+# end
