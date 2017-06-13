06dc8ec2b13b420b286e7f4adb5088b8f60db01a
Jeremy Yap <dcsyjlj@nus.edu.sg>
2017-06-13 13:37:41 +0800

    Move process pool terminate ensure block

--- main.rb
+++ main.rb
@@ -8,21 +8,20 @@ Dir[File.dirname(__FILE__) + '/tables/*.rb'].each { |file| require file }
 
 begin
   pool = ProcessPool.new(4)
+  course_ids = []
+
+  $url_mapper = UrlHashMapper.new
+  unless defined? Rails::Console
+    # UserMigrator.new.start
+    course_ids.each do |id|
+      pool.schedule do
+        logger = CourseLogger.new(id)
+        CourseMigrator.new(id, logger, concurrency: 1, fix_id: true).start
+      end
+    end
+
+    pool.wait
+  end
 ensure
   pool.terminate!
-end
-
-course_ids = []
-
-$url_mapper = UrlHashMapper.new
-unless defined? Rails::Console
-  # UserMigrator.new.start
-  course_ids.each do |id|
-    pool.schedule do
-      logger = CourseLogger.new(id)
-      CourseMigrator.new(id, logger, concurrency: 1, fix_id: true).start
-    end
-  end
-
-  pool.wait
 end
