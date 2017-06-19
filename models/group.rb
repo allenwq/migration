8652c0928a10f44870a903d6ed7c8502af88c6a8
WANG QIANG <rovingbreeze@gmail.com>
2017-06-19 10:42:50 +0800

    Fix the validation of group * Do not check if group or user is not present

--- models/group.rb
+++ models/group.rb
@@ -6,4 +6,11 @@ module V1
     def set_defaults
     end
   end
+
+  ::Course::GroupUser.class_eval do
+    def course_user_and_group_in_same_course
+      return if group.nil? || course_user.nil? || group.course == course_user.course
+      errors.add(:course_user, :not_enrolled)
+    end
+  end
 end
